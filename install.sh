cd4207e04936e2144fecf0a83dd504d30bc5a584
vide-sou <victorhugods.pereira@gmail.com>
2024-10-25 21:24:55 -0300

    commit

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+rm -rf ~/git-sync
 git clone git@github.com:victorhdsp/git-sync.git ~/git-sync
 cd ~/git-sync
-chmod 777 ~/git-sync/exec.sh
+chmod 777 "~/git-sync/exec.sh"
 echo "alias git-sync=~/git-sync/exec.sh" >> ~/.zshrc"
