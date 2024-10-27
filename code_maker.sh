9a3a2761a5f2618b452a2e7a1e6da641c7e2ad0a
vide-sou <victorhugods.pereira@gmail.com>
2024-10-27 12:21:22 -0300

    mudança no nome do pre-commit

--- code_maker.sh
+++ code_maker.sh
@@ -19,5 +19,4 @@ bash    $ROOT/scripts/$CLEAR_DIR $CLEAR_DIR $TARGET/$PROJECT
 bash    $ROOT/scripts/$CLONE_DIR $CLONE_DIR $TARGET/$PROJECT
 bash    $ROOT/scripts/$COMMIT $COMMIT $TARGET/$PROJECT
 
-bash    $ROOT/commands/pre-commit.sh pre-commit.sh $TARGET/$PROJECT
-chmod 777 $ROOT/commands/pre-commit.sh
+bash    $ROOT/commands/pre-commit.sh pre-commit $TARGET/$PROJECT
